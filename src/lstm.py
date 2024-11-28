821cc7abb7a6a7f40ba8c5714f1e424699d427cc
Adrià Lisa Bou <105350856+AdriaCoding@users.noreply.github.com>
2024-11-28 16:06:57 +0100

    Working with cuda is still too slow

--- src/lstm.py
+++ src/lstm.py
@@ -1,4 +1,8 @@
+import pandas as pd
+import numpy as np
 import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
 
-from torch.utils.data import Dataset
 
+df = pd.read_parquet('data/processed/2023_complete_pivot.parquet', parse_dates=['date'], index_col='date')
