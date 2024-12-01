99e23bd0f016e9534f4172313cb6bcb37c07aaea
Adrià Lisa Bou <105350856+AdriaCoding@users.noreply.github.com>
2024-12-01 14:45:27 +0100

    Cleanup code folder structures

--- src/config.py
+++
@@ -1,3 +0,0 @@
-RAW_DATA_PATH = 'data/raw/2023.parquet'
-PROCESSED_DATA_PATH = 'data/processed/2023_clean.parquet'
-IMPUTATION_METHOD = 'interpolation'  # Options: 'mean', 'median', 'interpolation'
--- src/lstm.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import torch
-from torch.utils.data import Dataset, DataLoader
-import torch.nn as nn
-
-
-df = pd.read_parquet('data/processed/2023_complete_pivot.parquet', parse_dates=['date'], index_col='date')
